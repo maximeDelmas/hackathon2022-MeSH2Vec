7b6d5f31c69ddac0f6ad2506ca5bdee6be95d685
maximeDelmas <maxime.delmas@hotmail.fr>
2022-06-02 15:57:22 +0200

    fix and new data

--- app/mesh2vec_dataset.py
+++ app/mesh2vec_dataset.py
@@ -18,7 +18,8 @@ class MeSH2VecDataset(Dataset):
     """
 
 
-    def __init__(self, f_input, f_target, f_vocab, f_labels):
+    def __init__(self, f_input, f_target, f_vocab, f_labels, checking=False):
+        self.vocab_checking = checking
         self.input = self.read(f_input).tolist()
         self.target = self.read(f_target).squeeze().tolist()
         self.vocab = self.read_vocab(f_vocab)
@@ -44,19 +45,20 @@ class MeSH2VecDataset(Dataset):
         v = self.read(f_vocab).squeeze().tolist()
 
         # Check that all input are in vocab:
-        flat_input = list(np.concatenate(self.input).flat)
-        if not all([j in v for j in flat_input]):
-            print("[WARNING] Some MeSH in input are not in the vocabulary")
-        
-        if not all([j in v for j in self.target]):
-            print("[WARNING] Some MeSH in target are not in the vocabulary")
-        
+        if self.vocab_checking:
+            flat_input = list(np.concatenate(self.input).flat)
+            if not all([j in v for j in flat_input]):
+                print("[WARNING] Some MeSH in input are not in the vocabulary")
+
+            if not all([j in v for j in self.target]):
+                print("[WARNING] Some MeSH in target are not in the vocabulary")
+
         return dict(zip(v, range(len(v))))
 
     def mesh2label_dict(self, f_labels):
         labels = self.read(f_labels)
         return dict(zip(labels[:, 0].tolist(), labels[:, 1].tolist()))
-    
+
     def tensor2labels(self, t):
         return [self.labels.get(self.inverse_vocab.get(int(i), "NaN"), "No label Found") for i in t]
 
--- app/prepare_data.py
+++ app/prepare_data.py
@@ -1,5 +1,10 @@
 import pandas as pd
 import collections
+import requests
+import sys
+import random
+import csv
+from io import StringIO
 
 
 def create_ancestors_dict(path_to_ancestors, path_to_weights=None):
@@ -36,6 +41,137 @@ def create_ancestors_dict(path_to_ancestors, path_to_weights=None):
 
 
 
+def extract_samples(url, n, max_int, tree):
 
-a = create_ancestors_dict("data/mesh_ancestors.csv", "data/mesh_pmids_count.csv")
+    # Estimate steps, limits and offset for sparql requests
+    n_steps = n // max_int
+    rest = n % max_int
+    offset_list = [i * max_int for i in range(n_steps)]
+    limit_list = [max_int] * n_steps
+    if (rest):
+        offset_list.append(n_steps * max_int)
+        limit_list.append(rest)
 
+    # Request parameters
+    header = {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Accept": "text/csv"
+    }
+
+    data = {
+        "format": "csv",
+    }
+
+    request = """
+    DEFINE input:inference "schema-inference-rules"
+
+    select distinct (strafter(str(?pmid), "http://rdf.ncbi.nlm.nih.gov/pubchem/reference/PMID") as ?PMID) (strafter(STR(?mesh),"http://id.nlm.nih.gov/mesh/") as ?MESH)
+    where
+    {
+        {
+            select ?pmid
+            where
+            {
+                {
+                    select distinct ?pmid
+                    where
+                    {
+                        ?pmid a fabio:Article .
+                        ?pmid dcterms:date ?date
+                    }
+                    ORDER BY DESC(?date)
+                }
+            }
+            LIMIT %(limit)s
+            OFFSET %(offset)s
+        }
+    ?pmid (fabio:hasSubjectTerm|fabio:hasSubjectTerm/meshv:hasDescriptor) ?mesh .
+    ?mesh a meshv:TopicalDescriptor .
+    ?mesh meshv:active 1 .
+    ?mesh meshv:treeNumber ?tn .
+    FILTER(REGEX(?tn,"%(tree)s")) .
+    }
+
+"""
+
+    dataset = pd.DataFrame()
+
+    for i in range(len(offset_list)):
+
+        # Send request
+        offset, limit = offset_list[i], limit_list[i]
+        print(f"Offset number {offset} with limit: {limit}")
+        filled_request = request % {"offset" : offset, "limit" : limit, "tree": tree}
+        data["query"] = filled_request
+        print(filled_request)
+        r = requests.post(url=url, headers=header, data=data)
+
+        # Check request status
+        if r.status_code != 200:
+            print("Error in request")
+            sys.exit(3)
+
+        # Convert string to dataframe
+        csvStringIO = StringIO(r.text)
+        df = pd.read_csv(csvStringIO, sep=",")
+        print(len(set(df["PMID"])))
+
+        # Concat dataframe to dataset
+        dataset = pd.concat([dataset, df])
+    
+    return dataset
+
+
+def create_samples(n, data, w, ancestors, f_input, f_target):
+    """
+    n(int): nombre d'exemples à générer
+    data(fichier format CSV): les pmids avec les mesh annotés
+    w(int): taille de la fenêtre, nombre de meshs pour le contexte
+    ancestors(dico {mesh:poids}): ancêtres du mesh dans arbre
+    f_input(chemin fichier CSV): fichier où enregistrer le contexte
+    f_target(chemin fichier CSV): fichier où enregistrer les target
+    """
+    
+    context = []
+    target = []
+    
+    df = pd.read_csv(data)
+    pmids = [id for id in df["PMID"].unique()]
+
+    i = 0
+    while i < n:
+        pmid = random.choice(pmids)
+        mesh = df[df["PMID"] == pmid]["MESH"].to_list()
+
+        # target = 1 mesh au hasard
+        mesh_target = mesh.pop(random.randrange(len(mesh)))
+        mesh_target = random.choices(ancestors[mesh_target]["ancestors"], weights=ancestors[mesh_target]["proba"], k=1)
+        target.append(mesh_target)
+
+        # contexte = w mesh au hasard
+        if w > len(mesh):
+            # supprimer
+            target.pop()
+        else:
+            mesh_context = [mesh.pop(random.randrange(len(mesh))) for j in range(w)]
+            mesh_context = [random.choices(ancestors[m]["ancestors"], weights=ancestors[m]["proba"], k=1)[0] for m in mesh_context]
+            context.append(mesh_context)
+            i += 1
+
+    with open(f_input, "w") as f:
+        write = csv.writer(f)
+        write.writerows(context)
+    with open(f_target, "w") as f:
+        write = csv.writer(f)
+        write.writerows(target)
+
+
+# sampling_100000_pmids_mesh = extract_samples(url="https://forum.semantic-metabolomics.fr/sparql", n=100000, max_int=10000, tree="(C|A|G|F|I|J|D20|D23|D26|D27)")
+# sampling_100000_pmids_mesh.to_csv("data/sampling_100000_pmids_mesh.csv", header=True, index=False)
+
+
+
+ancestors_dict = create_ancestors_dict("data/mesh_ancestors.csv", "data/mesh_pmids_count.csv")
+print(ancestors_dict["D010084"])
+
+create_samples(1000000, "data/sample_1000000/sampling_100000_pmids_mesh.csv", 6, ancestors_dict, "data/sample_1000000/context.csv", "data/sample_1000000/target.csv")
